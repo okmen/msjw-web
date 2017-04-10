2c2281b069e559f381af3a25b7f53feb45f9bb1c
liuminkang <liuminkang@chudaokeji.com>
2017-04-10 19:40:17 +0800

    Revert "个人中心17-26
    This reverts commit 37af867c0113b4476511781ab9eea60094494d7f.

--- src/main/java/cn/web/front/action/account/AccountAction.java
+++ src/main/java/cn/web/front/action/account/AccountAction.java
@@ -9,16 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.alibaba.fastjson.JSON;
 
 import cn.account.service.IAccountService;
 import cn.sdk.exception.ResultCode;
 import cn.sdk.util.StringUtil;
-import cn.user.bean.BaseBean;
 import cn.web.front.support.BaseAction;
 
 
@@ -29,7 +24,6 @@ import cn.web.front.support.BaseAction;
  * 
  */
 @Controller
-@RequestMapping(value="/user/")
 public class AccountAction extends BaseAction {
     private final static Logger logger = LoggerFactory.getLogger(AccountAction.class);
 
@@ -37,7 +31,7 @@ public class AccountAction extends BaseAction {
     @Qualifier("accountService")
     private IAccountService accountService;
 
-    @RequestMapping(value = "get-wechat-userInfo-by-id")
+    @RequestMapping(value = "/user/get-wechat-userInfo-by-id.html")
     public ModelAndView getWechatUserInfoById(HttpServletRequest request) {
         String version = request.getParameter("version");
         String idStr = request.getParameter("id");
@@ -61,168 +55,5 @@ public class AccountAction extends BaseAction {
         
         return getBaseView(request, modelMap, "/user/getWechatUserInfo.vm");
     }
-    
-    /**
-     * 用户中心-解绑微信
-     * @param identityCard 身份证号码
-     * @param openId  微信openId
-     * @param unionId 支付宝unionId
-     */
-    @RequestMapping(value = "deleteVehicle",method = RequestMethod.POST)
-    public void deleteVehicle(@RequestParam("identityCard") String identityCard,
-    		@RequestParam("openId") String openId,@RequestParam("unionId") String unionId) {
-    	
-    	
-    	
-    	BaseBean basebean = new  BaseBean();
-    	basebean.setCode("0000");
-    	basebean.setMsg("");   	    
-    	renderJSON(basebean);
-    	logger.info(JSON.toJSONString(basebean));
-    
-    }
-    
-    
-    
-    /**
-     * 用户中心-添加车辆(个人和他人)
-     * @param bindType 绑定类型
-     * @param vehicleType 车辆类型
-     * @param plateType 号牌种类
-     * @param licensePlateNumber 车牌号码
-     * @param frameNumber 车架号码
-     * @param ownerName   车主姓名  
-     * @param identityCard 身份证号
-     * @param mobilephone 手机号
-     */
-    @RequestMapping(value = "addVehicle",method = RequestMethod.POST)
-    public void addVehicle(@RequestParam("bindType") int bindType,@RequestParam("vehicleType") String vehicleType,
-    		@RequestParam("plateType") String plateType,@RequestParam("licensePlateNumber") String licensePlateNumber
-    		,@RequestParam("frameNumber") String frameNumber,@RequestParam("ownerName") String ownerName
-    		,@RequestParam("identityCard") String identityCard,@RequestParam("mobilephone") String mobilephone) {
-    	
-    	
-    	
-    	
-    	
-    	BaseBean basebean = new  BaseBean();
-    	basebean.setCode("0000");
-    	basebean.setMsg("");   	
-    	renderJSON(basebean);
-    	logger.info(JSON.toJSONString(basebean));
-    
-    }
-    
-    
-    /**
-     *我的资料-修改个人资料
-     * @param tureName 姓名
-     * @param identityCard 身份证号码
-     * @param mailingAddress 通讯地址
-     * @param idCardImgPositive 身份证正面
-     * @param idCardImgNegative 身份证反面
-     * @param IdCardImgHandHeld 手持身份证
-     */
-    @RequestMapping(value = "updateUser",method = RequestMethod.POST)
-    public void updateUser(@RequestParam("tureName") String tureName,@RequestParam("identityCard") String identityCard
-    		,@RequestParam("mailingAddress") String mailingAddress,@RequestParam("idCardImgPositive") String idCardImgPositive
-    		,@RequestParam("idCardImgNegative") String idCardImgNegative,@RequestParam("IdCardImgHandHeld") String IdCardImgHandHeld) {
-    	
-    	
-    	
-    	
-    	
-    	BaseBean basebean = new  BaseBean();
-    	basebean.setCode("0000");
-    	basebean.setMsg("");   	
-    	renderJSON(basebean);
-    	logger.info(JSON.toJSONString(basebean));
-    
-    }
-    
-    
-    
-    /**
-     * 我的资料-修改手机号
-     * @param oldMobile 旧手机号
-     * @param validateCode 验证码
-     * @param newMobile 新手机号
-     */
-    @RequestMapping(value = "updateMobile",method = RequestMethod.POST)
-    public void updateMobile(@RequestParam("oldMobile") String oldMobile,@RequestParam("validateCode") String validateCode
-    		,@RequestParam("newMobile") String newMobile) {
-    	
-    	
-    	
-    	
-    	
-    	BaseBean basebean = new  BaseBean();
-    	basebean.setCode("0000");
-    	basebean.setMsg("");   	
-    	renderJSON(basebean);
-    	logger.info(JSON.toJSONString(basebean));
-    
-    }
-    
-    
-    
-   /**
-    * 我的资料-修改密码
-    * @param oldPwd
-    * @param newPwd
-    */
-    @RequestMapping(value = "updatePwd",method = RequestMethod.POST)
-    public void updatePwd(@RequestParam("oldPwd") String oldPwd,@RequestParam("newPwd") String newPwd) {
-    	
-
-    	BaseBean basebean = new  BaseBean();
-    	basebean.setCode("0000");
-    	basebean.setMsg("");   	
-    	renderJSON(basebean);
-    	logger.info(JSON.toJSONString(basebean));
-    
-    }
-
-    
-    /**
-     * 随手拍举报-星级用户/普通用户
-     * @param illegalTime 违法时间
-     * @param illegalSections 违法路段
-     * @param idCardImgPositive 身份证正面
-     * @param idCardImgNegative 身份证反面
-     * @param IdCardImgHandHeld 手持身份证
-     * @param situationStatement 情况说明
-     * @param whistleblower 举报人
-     * @param identityCard 身份证
-     * @param mobilephone 联系电话
-     * @param locationAddress 定位地址
-     */
-    @RequestMapping(value = "readilyShoot",method = RequestMethod.POST)
-    public void readilyShoot(@RequestParam("illegalTime") String illegalTime,@RequestParam("illegalSections") String illegalSections    		
-    		,@RequestParam("idCardImgPositive") String idCardImgPositive,@RequestParam("idCardImgNegative") String idCardImgNegative
-    		,@RequestParam("IdCardImgHandHeld") String IdCardImgHandHeld,@RequestParam("situationStatement") String situationStatement
-    		,@RequestParam("whistleblower") String whistleblower,@RequestParam("identityCard") String identityCard
-    		,@RequestParam("mobilephone") String mobilephone,@RequestParam("locationAddress") String locationAddress) {
-    	
-    	
-    	
-    	
-    	
-    	BaseBean basebean = new  BaseBean();
-    	basebean.setCode("0000");
-    	basebean.setMsg("");   	
-    	Map<String, Object> modelMap = new HashMap<String, Object>();
-     	modelMap.put("recordNumber", "000000000");
-     	modelMap.put("queryPassword", "123456");
-     	basebean.setData(modelMap);
-    	renderJSON(basebean);
-    	logger.info(JSON.toJSONString(basebean));
-    
-    }
-    
-    
-    
-    
-    
 
 }
