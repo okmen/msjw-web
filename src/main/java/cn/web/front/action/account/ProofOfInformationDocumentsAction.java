99a2b82fcaa1129bdf7441cd90a9138df871b600
qinyongbo <qinyongbo@chudaokeji.com>
2017-04-10 16:00:01 +0800

    用户中心

---
+++ src/main/java/cn/web/front/action/account/ProofOfInformationDocumentsAction.java
@@ -0,0 +1,150 @@
+package cn.web.front.action.account;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.alibaba.fastjson.JSON;
+
+import cn.account.bean.vo.DriverLicenseInformationSheetVo;
+import cn.account.bean.vo.MotorVehicleInformationSheetVo;
+import cn.sdk.bean.BaseBean;
+import cn.web.front.support.BaseAction;
+
+/**
+ * 信息单据证明
+ * @author Mbenben
+ *
+ */
+@Controller
+@RequestMapping(value="/user/proofOfInformationDocuments")
+@SuppressWarnings(value="all")
+public class ProofOfInformationDocumentsAction extends BaseAction{
+	
+	/**
+	 * 提交驾驶证信息单
+	 * @param userName
+	 * @param identityCard
+	 * @param mobilephone
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * URL
+	 * http://localhost:8080/web/user/proofOfInformationDocuments/commitDriverLicenseInformationSheet.html?userName=张学良&identityCard=543333339&mobilephone=13666666666
+	 */
+    @RequestMapping(value="commitDriverLicenseInformationSheet",method=RequestMethod.POST)
+    public void commitDriverLicenseInformationSheet(@RequestParam("userName") String userName,@RequestParam("identityCard") String identityCard,@RequestParam("mobilephone") String mobilephone,
+    		HttpServletRequest request,HttpServletResponse response) throws IOException{
+    	BaseBean baseBean = new BaseBean();
+    	baseBean.setCode("0000");
+    	baseBean.setMsg("");
+    	
+    	MotorVehicleInformationSheetVo authenticationBasicInformation = new MotorVehicleInformationSheetVo();
+    	
+    	baseBean.setData("");
+    	renderJSON(baseBean);
+    	logger.info(JSON.toJSONString(baseBean));
+    }
+	/**
+	 * 获取驾驶证信息单
+	 * @param identityCard
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * URL
+	 * http://localhost:8080/web/user/proofOfInformationDocuments/getDriverLicenseInformationSheet.html?identityCard=543333339
+	 */
+    @RequestMapping(value="getDriverLicenseInformationSheet",method=RequestMethod.POST)
+    public void getDriverLicenseInformationSheet(@RequestParam("identityCard") String identityCard,
+    		HttpServletRequest request,HttpServletResponse response) throws IOException{
+    	BaseBean baseBean = new BaseBean();
+    	baseBean.setCode("0000");
+    	baseBean.setMsg("");
+    	DriverLicenseInformationSheetVo driverLicenseInformationSheetVo = new DriverLicenseInformationSheetVo();
+    	driverLicenseInformationSheetVo.setIdentityCard("431225199111123433");
+    	driverLicenseInformationSheetVo.setUserName("张小龙");
+    	driverLicenseInformationSheetVo.setMobilephone("13666666666");
+    	
+    	baseBean.setData(driverLicenseInformationSheetVo);
+    	renderJSON(baseBean);
+    	logger.info(JSON.toJSONString(baseBean));
+    }
+	
+	/**
+	 * 获取机动车信息单
+	 * @param identityCard
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * URL
+	 * http://localhost:8080/web//user/proofOfInformationDocuments/getMotorVehicleInformationSheet.html?identityCard=543333339
+	 */
+    @RequestMapping(value="getMotorVehicleInformationSheet",method=RequestMethod.POST)
+    public void getMotorVehicleInformationSheet(@RequestParam("identityCard") String identityCard,
+    		HttpServletRequest request,HttpServletResponse response) throws IOException{
+    	BaseBean baseBean = new BaseBean();
+    	baseBean.setCode("0000");
+    	baseBean.setMsg("");
+    	MotorVehicleInformationSheetVo motorVehicleInformationSheetVo = new MotorVehicleInformationSheetVo();
+    	motorVehicleInformationSheetVo.setIdentityCard("431225199111123433");
+    	motorVehicleInformationSheetVo.setUserName("张小龙");
+    	motorVehicleInformationSheetVo.setMobilephone("13666666666");
+    	motorVehicleInformationSheetVo.setProvinceAbbreviation("粤");
+    	
+    	List<String> numberPlateNumbers = new ArrayList<String>();
+    	numberPlateNumbers.add("B54349");
+    	numberPlateNumbers.add("B33337");
+    	
+    	List<String> plateTypes = new ArrayList<String>();
+    	plateTypes.add("小型轿车");
+    	plateTypes.add("小型专用客车");
+    	
+    	motorVehicleInformationSheetVo.setNumberPlateNumbers(numberPlateNumbers);
+    	motorVehicleInformationSheetVo.setPlateTypes(plateTypes);
+    	
+    	baseBean.setData(motorVehicleInformationSheetVo);
+    	renderJSON(baseBean);
+    	logger.info(JSON.toJSONString(baseBean));
+    }
+    
+	/**
+     * 提交机动车信息单
+     * @param userName 姓名
+     * @param identityCard 身份证号
+     * @param mobilephone 联系电话 
+     * @param provinceAbbreviation 车牌核发省简称 例如：粤
+     * @param numberPlateNumber 号牌号码 例如：B701NR
+     * @param plateType 车辆类型 例如:小型汽车
+     * @param request
+     * @param response
+     * @throws IOException
+     * URL 
+     * http://localhost:8080/web/user/proofOfInformationDocuments/commitMotorVehicleInformationSheet.html?userName=张小龙&identityCard=431225199222222222&mobilephone=13666666666&plateType=小型汽车&numberPlateNumber=B701NR&provinceAbbreviation=粤&
+     */
+    @RequestMapping(value="commitMotorVehicleInformationSheet",method=RequestMethod.POST)
+    public void commitMotorVehicleInformationSheet(@RequestParam("userName") String userName,@RequestParam("identityCard") String identityCard,
+    		@RequestParam("mobilephone") String mobilephone,@RequestParam("provinceAbbreviation") String provinceAbbreviation,
+    		@RequestParam("numberPlateNumber") String numberPlateNumber,@RequestParam("plateType") String plateType,
+    		HttpServletRequest request,HttpServletResponse response) throws IOException{
+    	
+    	BaseBean baseBean = new BaseBean();
+    	baseBean.setCode("0000");
+    	baseBean.setMsg("");
+    	
+    	MotorVehicleInformationSheetVo authenticationBasicInformation = new MotorVehicleInformationSheetVo();
+    	
+    	baseBean.setData("");
+    	
+    	renderJSON(baseBean);
+    	
+    	logger.info(JSON.toJSONString(baseBean));
+    }
+}
