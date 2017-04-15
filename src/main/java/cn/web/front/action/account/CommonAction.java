01dec6f8b84de77f2a7f1f2bc45af189478b61f8
qinyongbo <qinyongbo@chudaokeji.com>
2017-04-15 14:40:17 +0800

    添加 用户中心的action代码

---
+++ src/main/java/cn/web/front/action/account/CommonAction.java
@@ -0,0 +1,21 @@
+package cn.web.front.action.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.web.front.support.BaseAction;
+/**
+ * 公用Action
+ * 
+ * @author qinyongbo
+ * 
+ */
+@Controller
+@RequestMapping(value="/common/")
+@SuppressWarnings(value="all")
+public class CommonAction extends BaseAction {
+	//文件上传
+	
+	//获取须知文档
+	
+}
