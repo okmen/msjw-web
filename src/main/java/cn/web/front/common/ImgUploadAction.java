7c6a618d7a7eaa8933cf6d92490244a11c1a942f
zan yang <yangzhan@chudaokeji.com>
2017-04-18 14:10:25 +0800

    图片上传

--- src/main/java/cn/web/front/common/ImgUploadAction.java
+++ src/main/java/cn/web/front/common/ImgUploadAction.java
@@ -1,14 +1,25 @@
 package cn.web.front.common;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.io.FileUtils;
+import org.apache.velocity.texen.util.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import cn.file.service.IFileService;
 import cn.web.front.support.BaseAction;
 import net.sf.json.JSONObject;
 
@@ -23,6 +34,9 @@ import net.sf.json.JSONObject;
 @RequestMapping(value="/common")
 public class ImgUploadAction extends BaseAction{
 
+    @Autowired
+    @Qualifier("fileService")
+    private IFileService  fileService;
 	/**
 	 * @Title: imgUpload 
 	 * @Description: TODO(图片上传) 
@@ -32,7 +46,8 @@ public class ImgUploadAction extends BaseAction{
 	 * @throws
 	 */
 	@RequestMapping(value="/imgUpload.html")
-	public void imgUpload(HttpServletRequest request,HttpServletResponse response){
+	public void imgUpload(HttpServletRequest request,HttpServletResponse response,
+			@RequestParam(value="file",required=false) MultipartFile file){
 		PrintWriter out=null;
 		HashMap<String,Object> jsonMap = new HashMap<String, Object>();
 		
@@ -42,9 +57,22 @@ public class ImgUploadAction extends BaseAction{
 			response.setContentType("text/html;charset=UTF-8");
 			out = response.getWriter();
 			
+			//验证file是否为空
+			if (file.isEmpty()) {
+				jsonMap.put("code", "1001");
+				jsonMap.put("msg", "请选择上传图片！");
+				out.print(JSONObject.fromObject(jsonMap));
+				return;
+			}
+			
+			//转存文件  获取File
+			File file2 = multipartToFile(file);
+			//调用七牛上传图片  获得Url
+			String url = fileService.uploadFile(file2);
+			
 			jsonMap.put("code", "0000");
 			jsonMap.put("msg", "图片上传成功！");
-			jsonMap.put("imgUrl", "http://image.baidu.com/");
+			jsonMap.put("imgUrl", url);
 			out.print(JSONObject.fromObject(jsonMap));
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -53,4 +81,67 @@ public class ImgUploadAction extends BaseAction{
 			out.print(JSONObject.fromObject(jsonMap));
 		}
 	}
+	
+	
+	/**
+	 * @Title: getUpToken 
+	 * @Description: TODO(获取七牛上传凭证) 
+	 * @param @param request
+	 * @param @param response 设定文件 
+	 * @return void 返回类型 
+	 * @throws
+	 */
+	@RequestMapping(value="/getUpToken.html")
+	public void getUpToken(HttpServletRequest request,HttpServletResponse response){
+		PrintWriter out=null;
+		HashMap<String,Object> jsonMap = new HashMap<String, Object>();
+		
+		try {
+			request.setCharacterEncoding("UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
+			out = response.getWriter();
+			
+			String token = fileService.getUpToken();
+			jsonMap.put("code", "0000");
+			jsonMap.put("msg", "获取上传凭证成功！");
+			jsonMap.put("upToken", token);
+			out.print(JSONObject.fromObject(jsonMap));
+		} catch (Exception e) {
+			e.printStackTrace();
+			jsonMap.put("success", "1010");
+			jsonMap.put("msg", "服务器繁忙！");
+			out.print(JSONObject.fromObject(jsonMap));
+		}
+	}
+	
+	/**
+	 * @Title: multipartToFile 
+	 * @Description: TODO(multipartToFile) 
+	 * @param @param multfile
+	 * @param @return
+	 * @param @throws IOException 设定文件 
+	 * @return File 返回类型 
+	 * @throws
+	 */
+	private File multipartToFile(MultipartFile multfile) throws IOException {  
+		File file = null;
+		try {
+			CommonsMultipartFile cf = (CommonsMultipartFile)multfile;   
+	        //这个myfile是MultipartFile的  
+	        DiskFileItem fi = (DiskFileItem) cf.getFileItem();  
+	        file = fi.getStoreLocation();  
+	        
+	        //手动创建临时文件 
+//	        if(file.length() < CommonConstants.MIN_FILE_SIZE){  
+//	            File tmpFile = new File(System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") +   
+//	                    file.getName());  
+//	            multfile.transferTo(tmpFile);  
+//	            return tmpFile;  
+//	        }  
+		} catch (Exception e) {
+			throw new IOException("文件转换异常！");
+		}
+        return file;  
+    }  
 }
