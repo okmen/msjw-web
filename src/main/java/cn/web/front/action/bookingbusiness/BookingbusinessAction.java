bbe1ea0642b29fc03ea1ab598568a9815dfb8d35
wufan <wufan>
2017-07-12 17:55:52 +0800

    基础接口

--- src/main/java/cn/web/front/action/bookingbusiness/BookingbusinessAction.java
+++ src/main/java/cn/web/front/action/bookingbusiness/BookingbusinessAction.java
@@ -1,24 +1,368 @@
 package cn.web.front.action.bookingbusiness;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import cn.booking.business.bean.AppTimeHelper;
+import cn.booking.business.bean.BusinessTypeVO;
+import cn.booking.business.bean.IdTypeVO;
+import cn.booking.business.bean.OrgVO;
 import cn.booking.business.service.IBookingBusinessService;
+import cn.sdk.bean.BaseBean;
+import cn.sdk.util.MsgCode;
+import cn.sdk.util.StringUtil;
+import cn.web.front.support.BaseAction;
 
 /**
  * 预约类Action
+ * 
  * @author Mbenben
  *
  */
 @Controller
-@RequestMapping(value="/bookingbusiness/")
-@SuppressWarnings(value="all")
-public class BookingbusinessAction {
-	
+@RequestMapping(value = "/bookingbusiness/")
+@SuppressWarnings(value = "all")
+public class BookingbusinessAction extends BaseAction {
+
 	@Autowired
-    @Qualifier("bookingBusinessService")
-    private IBookingBusinessService bookingBusinessService;
-	
+	@Qualifier("bookingBusinessService")
+	private IBookingBusinessService bookingBusinessService;
+
+	/**
+	 * 获取车辆类型Id
+	 * 
+	 * @param code
+	 *            车辆类型code
+	 */
+	@RequestMapping("getCarTypeId")
+	public void getCarTypeId(String code) {
+		BaseBean baseBean = new BaseBean();
+		boolean flag = false;
+		if (StringUtil.isBlank(code)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("车牌类型code不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		try {
+			List<IdTypeVO> idTypeVOs = bookingBusinessService.getCarTypes();
+			if (null != idTypeVOs) {
+				baseBean.setCode(MsgCode.success);
+				for (IdTypeVO idTypeVO : idTypeVOs) {
+					String code2 = idTypeVO.getCode();
+					String id = idTypeVO.getId();
+					if (code.equals(code2)) {
+						flag = true;
+						baseBean.setData(id);
+					}
+				}
+				if (flag = false) {
+					baseBean.setCode(MsgCode.businessError);
+					baseBean.setMsg("查询车辆类型id失败");
+				}
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("查询车辆类型id失败");
+			}
+
+		} catch (Exception e) {
+			logger.error("查询车辆类型id异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
+	/**
+	 * 获取业务类型Id
+	 * 
+	 * @param code
+	 * @param type
+	 * @param part
+	 * @param arg0
+	 * @param arg1
+	 */
+
+	@RequestMapping("getBusinessTypeId")
+	public void getBusinessTypeId(String code, String type, String part, String arg0, String arg1) {
+		BaseBean baseBean = new BaseBean();
+		boolean flag = false;
+		if (StringUtil.isBlank(code)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("车牌类型code不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(type)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("预约类型不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(type)) {
+			type = "";
+		}
+		try {
+			List<BusinessTypeVO> businessTypes = bookingBusinessService.getBusinessTypes(type, part, arg0, arg1);
+			if (null != businessTypes) {
+				baseBean.setCode(MsgCode.success);
+				for (BusinessTypeVO businessTypeVO : businessTypes) {
+					String code2 = businessTypeVO.getCode();
+					String id = businessTypeVO.getId();
+					if (code.equals(code2)) {
+						flag = true;
+						baseBean.setData(id);
+					}
+				}
+				if (flag = false) {
+					baseBean.setCode(MsgCode.businessError);
+					baseBean.setMsg("查询业务类型id失败");
+				}
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("查询业务类型id失败");
+			}
+
+		} catch (Exception e) {
+			logger.error("查询业务类型id异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
+	/**
+	 * 获取证件种类Id
+	 * 
+	 * @param code
+	 * @param businessTypeId
+	 * @param arg0
+	 * @param arg1
+	 */
+
+	@RequestMapping("getIdTypeId")
+	public void getIdTypeId(String code, String businessTypeId, String arg0, String arg1) {
+		BaseBean baseBean = new BaseBean();
+		boolean flag = false;
+		if (StringUtil.isBlank(code)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("证件种类code不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(businessTypeId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("businessTypeId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		try {
+			List<IdTypeVO> idTypeVOs = bookingBusinessService.getIdTypes(businessTypeId, arg0, arg1);
+			if (null != idTypeVOs) {
+				baseBean.setCode(MsgCode.success);
+				for (IdTypeVO idTypeVO : idTypeVOs) {
+					String code2 = idTypeVO.getCode();
+					String id = idTypeVO.getId();
+					if (code.equals(code2)) {
+						flag = true;
+						baseBean.setData(id);
+					}
+				}
+				if (flag = false) {
+					baseBean.setCode(MsgCode.businessError);
+					baseBean.setMsg("查询车辆类型id失败");
+				}
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("查询车辆类型id失败");
+			}
+		} catch (Exception e) {
+			logger.error("获取证件种类id异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
+	/**
+	 * 获取预约地点
+	 * 
+	 * @param businessTypeId
+	 * @param arg0
+	 * @param arg1
+	 */
+
+	@RequestMapping("getOrgsByBusinessTypeId")
+	public void getOrgsByBusinessTypeId(String businessTypeId, String arg0, String arg1) {
+		BaseBean baseBean = new BaseBean();
+		if (StringUtil.isBlank(businessTypeId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("businessTypeId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		try {
+			List<OrgVO> orgVOs = bookingBusinessService.getOrgsByBusinessTypeId(businessTypeId, arg0, arg1);
+			if (null != orgVOs) {
+				baseBean.setCode(MsgCode.success);
+				baseBean.setData(orgVOs);
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("查询车辆类型id失败");
+			}
+
+		} catch (Exception e) {
+			logger.error("查询业务类型id异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
+	/**
+	 * 根据单位ID获取单位详细信息
+	 * 
+	 * @param orgId
+	 */
+	@RequestMapping("findOrgByOrgId")
+	public void findOrgByOrgId(String orgId) {
+		BaseBean baseBean = new BaseBean();
+		if (StringUtil.isBlank(orgId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("orgId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		try {
+			OrgVO orgVO = bookingBusinessService.findOrgByOrgId(orgId);
+			if (null != orgVO) {
+				baseBean.setCode(MsgCode.success);
+				baseBean.setData(orgVO);
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("根据单位ID获取单位详细信息失败");
+			}
+
+		} catch (Exception e) {
+			logger.error("根据单位ID获取单位详细信息异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
+	/**
+	 * 获取可预约日期
+	 * 
+	 * @param orgId
+	 * @param businessTypeId
+	 * @param arg0
+	 * @param arg1
+	 */
+	@RequestMapping("getAppointmentDate")
+	public void getAppointmentDate(String orgId, String businessTypeId, String arg0, String arg1) {
+		BaseBean baseBean = new BaseBean();
+		if (StringUtil.isBlank(orgId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("orgId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(businessTypeId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("businessTypeId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		try {
+			List<String> appointmentDates = bookingBusinessService.getAppointmentDate(orgId, businessTypeId, arg0,
+					arg1);
+			if (null != appointmentDates) {
+				baseBean.setCode(MsgCode.success);
+				baseBean.setData(appointmentDates);
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("获取可预约日期失败");
+			}
+
+		} catch (Exception e) {
+			logger.error("获取可预约日期异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
+	/**
+	 * 获取可预约时间段
+	 * 
+	 * @param date
+	 * @param orgId
+	 * @param businessTypeId
+	 * @param carTypeId
+	 * @param optlittleCar
+	 */
+
+	@RequestMapping("getAppTimes")
+	public void getAppTimes(String date, String orgId, String businessTypeId, String carTypeId, String optlittleCar) {
+		BaseBean baseBean = new BaseBean();
+		if (StringUtil.isBlank(date)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("date不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(orgId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("orgId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(businessTypeId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("businessTypeId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(carTypeId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("carTypeId不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		if (StringUtil.isBlank(businessTypeId)) {
+			baseBean.setCode(MsgCode.paramsError);
+			baseBean.setMsg("optlittleCar不能为空!");
+			renderJSON(baseBean);
+			return;
+		}
+		try {
+			List<AppTimeHelper> appTimes = bookingBusinessService.getAppTimes(date, orgId, businessTypeId, carTypeId,
+					optlittleCar);
+			if (null != appTimes) {
+				baseBean.setCode(MsgCode.success);
+				baseBean.setData(appTimes);
+			} else {
+				baseBean.setCode(MsgCode.businessError);
+				baseBean.setMsg("获取可预约时间段失败");
+			}
+
+		} catch (Exception e) {
+			logger.error("获取可预约时间段异常:" + e);
+			DealException(baseBean, e);
+		}
+		renderJSON(baseBean);
+		logger.debug(JSON.toJSONString(baseBean));
+	}
+
 }
-	
