1ede34b68622ef8264bc281f7fea7f6bcf1d56df
qinyongbo <qinyongbo@chudaokeji.com>
2017-07-12 14:57:03 +0800

    add

--- src/main/java/cn/web/front/action/bookingbusiness/BookingbusinessAction.java
+++ src/main/java/cn/web/front/action/bookingbusiness/BookingbusinessAction.java
@@ -1,11 +1,23 @@
 package cn.web.front.action.bookingbusiness;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import cn.booking.business.service.IBookingBusinessService;
+
 /**
  * 预约类Action
  * @author Mbenben
  *
  */
+@Controller
+@RequestMapping(value="/bookingbusiness/")
+@SuppressWarnings(value="all")
 public class BookingbusinessAction {
-	public void test(){
-		System.out.println("test");
-	}
+	
+	@Autowired
+    @Qualifier("bookingBusinessService")
+    private IBookingBusinessService bookingBusinessService;
+	
 }
