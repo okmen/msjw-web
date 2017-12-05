bca25116785207e7d7d611253e7a7dd8f3aff413
jiangjiayi <jiangjiayi@chudaokeji.com>
2017-12-05 17:06:38 +0800

    msjw接口

--- src/main/java/cn/web/front/action/convenience/MsjwAction.java
+++ src/main/java/cn/web/front/action/convenience/MsjwAction.java
@@ -1,32 +1,20 @@
 package cn.web.front.action.convenience;
 
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
-import cn.convenience.bean.ConvenienceBean;
-import cn.convenience.bean.FeedbackResultBean;
-import cn.convenience.service.IConvenienceService;
-import cn.message.service.ITemplateMessageService;
+import cn.convenience.service.IMsjwService;
 import cn.sdk.bean.BaseBean;
-import cn.sdk.exception.WebServiceException;
 import cn.sdk.util.MsgCode;
 import cn.sdk.util.StringUtil;
 import cn.web.front.support.BaseAction;
-import net.sf.json.JSONObject;
 
 /**
  * 
@@ -42,8 +30,8 @@ public class MsjwAction extends BaseAction{
 	private final static Logger logger = LoggerFactory.getLogger(MsjwAction.class);
 
     @Autowired
-    @Qualifier("convenienceService")
-    private IConvenienceService convenienceService;
+    @Qualifier("msjwService")
+    private IMsjwService msjwService;
     
     /**
      * 获取民生警务个人信息
@@ -51,9 +39,15 @@ public class MsjwAction extends BaseAction{
      * @param sourceOfCertification 认证来源
      */
     @RequestMapping("getMSJWinfo")
-    public void getMSJWinfo(String identityCard, String sourceOfCertification){
+    public void getMSJWinfo(String openId, String identityCard, String sourceOfCertification){
     	BaseBean baseBean = new BaseBean();	
     	try{
+    		if(StringUtil.isBlank(openId)){
+	 			baseBean.setCode(MsgCode.paramsError);
+	 			baseBean.setMsg("openId不能为空!");
+	 			renderJSON(baseBean);
+	 			return;
+	      	}
 	    	if(StringUtil.isBlank(identityCard)){
 	 			baseBean.setCode(MsgCode.paramsError);
 	 			baseBean.setMsg("identityCard不能为空!");
@@ -74,7 +68,17 @@ public class MsjwAction extends BaseAction{
 	    		}
 	    	}
 	    	
-	    	baseBean = convenienceService.getMSJWinfo(identityCard, sourceOfCertification);
+	    	//校验是否为民生警务平台合法用户
+	    	JSONObject json = msjwService.checkIsValidUser(openId, identityCard);
+	    	String code = json.getString("code");
+	    	
+	    	if("200".equals(code)){
+	    		//用户已登录，拉取警视通用户信息
+	    		baseBean = msjwService.getMSJWinfo(identityCard, sourceOfCertification);
+	    	}else{
+	    		baseBean.setCode(code);
+	    		baseBean.setMsg(json.getString("message"));
+	    	}
 	    	
 		} catch (Exception e) {
 			logger.error("【民生警务】getMSJWinfo接口Action异常: identityCard = " + identityCard + ",sourceOfCertification = " + sourceOfCertification);
