1ba396b17fb7ea4813a151a0bef6c82486a729e4
Your Name <you@example.com>
2018-01-22 21:17:54 +0800

    深圳交警投票

--- src/main/java/cn/web/front/task/SetOpenId4VoteTask.java
+++ src/main/java/cn/web/front/task/SetOpenId4VoteTask.java
@@ -6,7 +6,10 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+import cn.message.service.IWechatService;
 import cn.sdk.util.HttpClientUtil;
 
 /**
@@ -14,7 +17,11 @@ import cn.sdk.util.HttpClientUtil;
  */
 public class SetOpenId4VoteTask implements Runnable, Serializable {
 	private final static Logger logger = LoggerFactory.getLogger(SetOpenId4VoteTask.class);
-
+	
+	@Autowired
+	@Qualifier("wechatService")
+	private IWechatService wechatService;
+	
 	private String openId;
 	
 	public SetOpenId4VoteTask(String openId){
@@ -23,11 +30,12 @@ public class SetOpenId4VoteTask implements Runnable, Serializable {
 	
 	public void run() {
 		try {
-			//HttpRequest.sendPost("http://testjava.chudaokeji.com/wechat/setAuthOpenid.html", "openId=", 3000);
+		/*	HttpRequest.sendPost("http://testjava.chudaokeji.com/wechat/setAuthOpenid.html", "openId=", 3000);
 			Map<String, String> map = new HashMap<>();
 			map.put("openId", openId);
-//			HttpClientUtil.post("http://testjava.chudaokeji.com/wechat/setAuthOpenid.html", map, null);
-			HttpClientUtil.post("http://gzh.stc.gov.cn/api/wechat/setAuthOpenid.html", map, null);
+			HttpClientUtil.post("http://testjava.chudaokeji.com/wechat/setAuthOpenid.html", map, null);
+			HttpClientUtil.post("http://gzh.stc.gov.cn/api/wechat/setAuthOpenid.html", map, null);*/
+			wechatService.setAuthOpenid(openId);
 		} catch (Exception e) {
 			logger.error("SetOpenId4VoteTask 异常:",e);
 		}
